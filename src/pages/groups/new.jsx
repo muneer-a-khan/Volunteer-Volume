05d42cbde9178cf463fb08cb73b39225bbb5f63a
Muneer Khan <muneerkhan992000@gmail.com>
2025-04-26 02:23:23 -0400

    Created page for new group

--- src/pages/groups/new.jsx
+++ src/pages/groups/new.jsx
@@ -1,1 +1,17 @@
- 
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { GroupProvider } from '../../contexts/GroupContext';
+
+// Import the component dynamically with SSR disabled
+const NewGroupForm = dynamic(
+  () => import('../../components/groups/NewGroupForm'),
+  { ssr: false }
+);
+
+export default function NewGroupPage() {
+  return (
+    <GroupProvider>
+      <NewGroupForm />
+    </GroupProvider>
+  );
+} 
